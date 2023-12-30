7dc9310e23edf5da71d3a2751cc21d3999ef5bf0
Hiroki Ishiwatari <dev.ish1r0k1@gmail.com>
2023-12-30 15:29:46 +0900

    update 403, 404 and 500 pages

--- src/app/pages/forbidden-page/forbidden-page.component.ts
+++ src/app/pages/forbidden-page/forbidden-page.component.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-forbidden-page',
   standalone: true,
-  imports: [],
+  imports: [NzResultModule, RouterLinkWithHref, NzButtonModule],
   templateUrl: './forbidden-page.component.html',
   styleUrl: './forbidden-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex min-h-screen w-screen items-center justify-center',
+  },
 })
 export class ForbiddenPageComponent {}
