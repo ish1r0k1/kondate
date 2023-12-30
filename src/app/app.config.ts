2e5ed8ad2475e8d0545fa3c3ed7cd1284183a8bf
Hiroki Ishiwatari <dev.ish1r0k1@gmail.com>
2023-12-30 12:09:47 +0900

    setup formly

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,25 @@ import { routes } from './app.routes';
 import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import es from '@angular/common/locales/es';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyNgZorroAntdModule } from '@ngx-formly/ng-zorro-antd';
 
 registerLocaleData(es);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNzI18n(es_ES), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideNzI18n(es_ES),
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    importProvidersFrom(
+      ReactiveFormsModule,
+      FormlyModule,
+      FormlyNgZorroAntdModule
+    ),
+  ],
 };
