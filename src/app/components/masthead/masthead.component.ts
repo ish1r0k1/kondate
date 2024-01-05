9829d7b7f8787e618458685d93a0cbe76b58729e
Hiroki Ishiwatari <dev.ish1r0k1@gmail.com>
2024-01-05 18:32:37 +0900

    feat: update layout

--- src/app/components/masthead/masthead.component.ts
+++ src/app/components/masthead/masthead.component.ts
@@ -1,16 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-masthead',
   standalone: true,
-  imports: [NzAvatarModule, NzIconModule],
+  imports: [NzAvatarModule, RouterLinkWithHref],
   templateUrl: './masthead.component.html',
   styleUrl: './masthead.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    class: 'block border-b border-solid',
-  },
 })
 export class MastheadComponent {}
